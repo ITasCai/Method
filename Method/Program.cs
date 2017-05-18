e8175c4f7d892f8319ed8c3ac7019206ede8f16a
孤独的蔬菜 <shuai19970217@163.com>
2017-05-18 14:30:35 +0800

    方法

--- Method/Program.cs
+++ Method/Program.cs
@@ -10,9 +10,34 @@ namespace Method
     {
         static void Main(string[] args)
         {
-            GetName("狗蛋");
+            //  GetName("狗蛋");
+            Console.WriteLine("请输入第一个数：");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("请输入第二个数：");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int max= GrtMax(num1,num2);
+            Console.WriteLine("最大值为：{0}",max);
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// 判断两个数的最大值
+        /// </summary>
+        /// <param name="v1">值1</param>
+        /// <param name="v2">值2</param>
+        private static int GrtMax(int v1, int v2)
+        {
+            if (v1>v2)
+            {
+                return v1;
+            }
+            else
+            {
+                return v2;
+            }
+        }
+
         /// <summary>
         /// 有参数的方法
         /// </summary>
