8843e681ba3b268272d1a60f2a43580fae97d5a7
孤独的蔬菜 <shuai19970217@163.com>
2017-05-18 11:52:40 +0800

    方法

--- Method/Program.cs
+++ Method/Program.cs
@@ -10,6 +10,17 @@ namespace Method
     {
         static void Main(string[] args)
         {
+            GetName("狗蛋");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 有参数的方法
+        /// </summary>
+        /// <param name="name">姓名</param>
+
+        private static void GetName(string name)
+        {
+            Console.WriteLine("欢迎您:{0}",name);
         }
     }
 }
